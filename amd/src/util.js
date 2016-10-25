dfb0d04b92e72144740ca27ec050e8b52d69fefe
Hittesh Ahuja <hitteshahuja@gmail.com>
2016-10-25 23:29:12 +0100

    Moved toggle function here Added MAIN_SELECTOR Added reload_stylesheet

--- amd/src/util.js
+++ amd/src/util.js
@@ -1,5 +1,29 @@
 define(['jquery', 'block_accessibility/accessibility', 'core/config'], function ($, accessibility, config) {
     return {
+        MAIN_SELECTOR: '#page',
+        /**
+         * Enables or disables the buttons as specified
+         *
+         * @requires webroot
+         *
+         * @param {String} id the ID of the button to enable/disable
+         * @param {String} op the operation we're doing, either 'on' or 'off'.
+         *
+         */
+        toggle:function(id,op){
+            var button = $('#block_accessibility_'+id);
+            if (op == 'on') {
+                if (button.hasClass('disabled')) {
+                    //this.log('Enabling '+button);
+                    button.removeClass('disabled');
+                }
+            } else if (op == 'off') {
+                if(!button.hasClass('disabled')) {
+                    //this.log('Disabling '+button);
+                    button.addClass('disabled');
+                }
+            }
+        },
         sendRequest: function (location, data, successfn, failure) {
             $.ajax({
                 url: location,
@@ -40,6 +64,48 @@ define(['jquery', 'block_accessibility/accessibility', 'core/config'], function 
             if (this.transactionsCount == 0) {
                 $('#loader-icon').css('display', 'none');
                 $('#accessibility_controls').css('opacity', '1');
+            }
+        },
+        reload_stylesheet: function(id){
+            console.log("reloading stylesheet in util...");
+            var sheetnode = $('link[href="'+config.wwwroot+
+                '/blocks/accessibility/userstyles.php?instance_id='+id+'"]');
+            console.log(sheetnode);
+            var cache_prevention_salt = new Date().getTime();
+            var oldStylesheet = sheetnode;
+            var newStylesheet = null;
+            var cssURL = config.wwwroot+
+                '/blocks/accessibility/userstyles.php?instance_id='+
+                id+
+                '&v='+cache_prevention_salt;
+            if (document.createStyleSheet) // only for IE < 11 and IE > 8
+            {
+                /*
+                 here we use href attribute change which makes some delay while reloading stylesheet
+
+                 1. one another idea would be to load stylesheet using this.Y.io(.. and create <style> element
+                 var styleSheet = document.createElement('STYLE');
+                 document.documentElement.firstChild.appendChild(styleSheet);
+
+                 2. initial idea was the same as for non-IE browsers but somehow doesn't work:
+                 oldStylesheet.remove(true);
+                 newStylesheet = document.createStyleSheet(cssURL);
+                 // also keep in mind that createStyleSheet can create up to 31 stylesheets
+                 // http://msdn.microsoft.com/en-us/library/ie/ms531194(v=vs.85).aspx
+                 */
+                oldStylesheet.attr('href',cssURL);
+            }
+            else{
+                // IE 11 and non-IE browsers:
+                // creating new stylesheet and deleting old one makes more smooth transition
+                // Why wouldn't we just set the href attribute insted of creating another stylesheet node? Because before the new stylesheet is loaded and while old one is deleted, the page will lose all the styles and all the elements get unstyled for a some time (poor user experience)
+                newStylesheet = oldStylesheet.clone(true);
+                newStylesheet.attr('href', cssURL);
+                $('head').append(newStylesheet);
+                // remove old stylesheet
+                newStylesheet.load(function(){
+                    oldStylesheet.remove();
+                });
             }
         }
 
