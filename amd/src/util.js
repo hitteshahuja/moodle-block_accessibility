941285d982e807e2c0be163e885dc09a166bc9a2
Hittesh Ahuja <hitteshahuja@gmail.com>
2016-10-31 23:16:40 +0000

    Reload stylesheet works on the fly now

--- amd/src/util.js
+++ amd/src/util.js
@@ -1,4 +1,6 @@
 define(['jquery', 'block_accessibility/accessibility', 'core/config'], function ($, accessibility, config) {
+    var newStylesheet = null;
+    var sheetnode = null;
     return {
         MAIN_SELECTOR: '#page',
         /**
@@ -68,18 +70,17 @@ define(['jquery', 'block_accessibility/accessibility', 'core/config'], function 
         },
         reload_stylesheet: function(id){
             console.log("reloading stylesheet in util...");
-            var sheetnode = $('link[href="'+config.wwwroot+
-                '/blocks/accessibility/userstyles.php?instance_id='+id+'"]');
-            console.log(sheetnode);
+
             var cache_prevention_salt = new Date().getTime();
-            var oldStylesheet = sheetnode;
-            var newStylesheet = null;
+            var oldStylesheet = this.sheetnode;
+            console.log(oldStylesheet);
             var cssURL = config.wwwroot+
                 '/blocks/accessibility/userstyles.php?instance_id='+
                 id+
                 '&v='+cache_prevention_salt;
             if (document.createStyleSheet) // only for IE < 11 and IE > 8
             {
+                console.log("Reloading stylesheet");
                 /*
                  here we use href attribute change which makes some delay while reloading stylesheet
 
@@ -96,16 +97,21 @@ define(['jquery', 'block_accessibility/accessibility', 'core/config'], function 
                 oldStylesheet.attr('href',cssURL);
             }
             else{
+                console.log("creating new stylesheet and deleting old one makes more smooth transition");
                 // IE 11 and non-IE browsers:
                 // creating new stylesheet and deleting old one makes more smooth transition
                 // Why wouldn't we just set the href attribute insted of creating another stylesheet node? Because before the new stylesheet is loaded and while old one is deleted, the page will lose all the styles and all the elements get unstyled for a some time (poor user experience)
                 newStylesheet = oldStylesheet.clone(true);
                 newStylesheet.attr('href', cssURL);
+                console.log(newStylesheet);
                 $('head').append(newStylesheet);
+                console.log("appending new ss..");
                 // remove old stylesheet
-                newStylesheet.load(function(){
+               newStylesheet.load(function(){
+                    console.log("removing old ss..");
                     oldStylesheet.remove();
                 });
+                this.sheetnode = newStylesheet;
             }
         }
 
