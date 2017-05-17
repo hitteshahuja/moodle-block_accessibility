178b61e9644f37d1ea7d1a8a820eaaa40c1b0f45
Hittesh Ahuja <hitteshahuja@gmail.com>
2017-05-17 22:33:33 +0100

    Requires Moodle 3.1

--- version.php
+++ version.php
@@ -32,7 +32,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2015011600;
 $plugin->cron = 3600;
-$plugin->requires = 2011120500;
+$plugin->requires = 2016051900;
 $plugin->component = 'block_accessibility';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '2.2.3';
